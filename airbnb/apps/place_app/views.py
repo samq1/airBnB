df29c9410261981e70d76e15c4eccf6954b937d8
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-24 14:00:19 -0500

    updated CSS

--- airbnb/apps/place_app/views.py
+++ airbnb/apps/place_app/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render
 
-# Create your views here.
+def show_place(request):
+    # if "user_id" in request.session:
+    #     return redirect('/users/all')
+    # else:
+    #     return redirect('/users')
+    return render(request, "place/show_place.html")
+
+
+def new_page(request):
+    # if "user_id" in request.session:
+    #     return redirect('/users/all')
+    # else:
+    #     return redirect('/users')
+    return render(request, "base/new.html")
