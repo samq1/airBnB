3640cc1fd6d6b13cb2ea20ee4c22c3f0da96dfb1
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-24 16:09:30 -0500

    new revisions merge

--- airbnb/airbnb/settings.py
+++ airbnb/airbnb/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join("/apps/main_router/", "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
