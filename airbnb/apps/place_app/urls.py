3bad24a09a1d40f9b150214332b4943219efcb49
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-25 02:05:14 -0500

    completed new place show page and initial work on the show all places page

--- airbnb/apps/place_app/urls.py
+++ airbnb/apps/place_app/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from . import views #'.' means the same folder
 
 urlpatterns = [
-    url(r'^show', views.show_place),
+    url(r'filter(?:/ordering=(?P<criteria>(.)+))', views.filter_all, name="filter"),
+    url(r'^(?P<place_id>\d+)$', views.show_place, name="show"),
+    url(r'^does_not_exist$', views.does_not_exist, name="does_not_exist"),
 ]
--- airbnb/apps/place_app/views.py
+++ airbnb/apps/place_app/views.py
@@ -1,20 +1,55 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django_filters.rest_framework import DjangoFilterBackend
 from ..loginregis_app.models import *
 
 
-def show_place(request):
+def show_place(request, place_id):
     # if "user_id" in request.session:
     #     return redirect('/users/all')
     # else:
     #     return redirect('/users')
+    try:
+        place = Place.objects.get(id=place_id)
+    except:
+        return redirect(reverse('places:does_not_exist'))
     context = {
-        "places": Place.objects.all(),
         "users": User.objects.all(),
-        "place1": Place.objects.first()
+        "place1": place
     }
     return render(request, "place/show_place.html", context)
+
+
+def filter_all(request, criteria):
+    # if "user_id" in request.session:
+    #     return redirect('/users/all')
+    # else:
+    #     return redirect('/users')
+
+    if criteria == "price":
+        places = Place.objects.order_by('price_night')
+    elif criteria == "-price":
+        places = Place.objects.order_by('-price_night')
+    else:
+        places = Place.objects.order_by('price_night')
+
+    context = {
+        "places": places,
+        "locations": Place.objects.order_by(
+            'city').values('city', 'state').distinct(),
+        "states": Place.objects.order_by('state').values('state').distinct(),
+    }
+    return render(request, "place/filter_all.html", context)
+
+
+def does_not_exist(request):
+    # if "user_id" in request.session:
+    #     return redirect('/users/all')
+    # else:
+    #     return redirect('/users')
+    return render(request, "place/does_not_exist.html")
 
 
 def new_page(request):
