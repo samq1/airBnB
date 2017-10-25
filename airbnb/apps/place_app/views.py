1a6d2339206dd2cdb10ea328ffb422bd39773bdd
Joshua Daniel <joshuadaniel@fuller.edu>
2017-10-24 19:16:15 -0500

    new merge

--- airbnb/apps/place_app/views.py
+++ airbnb/apps/place_app/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
+from ..loginregis_app.models import *
 
 
 def show_place(request):
@@ -8,12 +9,17 @@ def show_place(request):
     #     return redirect('/users/all')
     # else:
     #     return redirect('/users')
-    return render(request, "place/show_place.html")
+    context = {
+        "places": Place.objects.all(),
+        "users": User.objects.all(),
+        "place1": Place.objects.first()
+    }
+    return render(request, "place/show_place.html", context)
 
 
 def new_page(request):
-    # if "user_id" in request.session:
-    #     return redirect('/users/all')
-    # else:
-    #     return redirect('/users')
+    if "user_id" in request.session:
+        return redirect('/users/all')
+    else:
+        return redirect('/users')
     return render(request, "base/new.html")
